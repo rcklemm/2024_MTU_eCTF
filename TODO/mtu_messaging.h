c2f0d6d07716db951486642801d3f6e8d385033a
Ryan Klemm <rcklemm@mtu.edu>
2024-02-15 00:46:39 -0500

    small change to messaging header

--- TODO/mtu_messaging.h
+++ TODO/mtu_messaging.h
@@ -10,12 +10,16 @@ simple_i2c and board_link libraries as-is, no need to handle that stuff ourselve
 #include "crypto_util.h"
 
 
-// Calculate this as MAX_I2C_LEN - (everything that isn't the contents part that is sent over I2C)
+// Calculate this as MAX_I2C_LEN - (everything that isn't the contents part that is sent over I2C) - 1
 #define MAX_CONTENTS_LEN 180
 #define HASH_LEN 32
 #define IV_LEN 16
 
 typedef struct msg_t {
+    // The existing code assumes this exists for telling AP / Components which operation the 
+    // message is doing (command_message struct in ap/component .c files)
+    uint8_t opcode;
+
     uint64_t rng_chal;
     uint64_t rng_resp;
 
