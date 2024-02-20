d505aa1db01425770d90839746f48aca1a3e6bde
The-No-rush <ethan.norush@protonmail.com>
2024-02-19 19:11:14 -0500

    Added untested time_delay function

--- TODO/general_util.c
+++ TODO/general_util.c
@@ -26,6 +26,33 @@ uint64_t rng_gen()
     return rnd64;
 }
 
-void time_delay(size_t low_us, size_t high_us);
+void time_delay(size_t low_us, size_t high_us)
+{
+    uint32_t rnd32;
+    size_t difference = high_us - low_us;
+    float modifier;
+    size_t delay;
+
+    //Initialize TRNG
+    MXC_TRNG_Init();
+
+    //Generate 32-bit number 
+    rnd32 = MXC_TRNG_RandomInt();
+
+    //Remove the divide by zero possiblity and it shouldn't affect code unless large numbers
+    if(rnd32 == 0){
+        rnd32 = 1;
+    }
+
+    //Dividing by the max uint32_t value can be to get the random modifier when multiplied by the difference
+    modifier = (rnd32 / 4,294,967,295) * difference;
+    //Rounds the modifier and is added to low time to get the delay
+    delay = low_us + roundf(modifier);
+    
+    //Puts the current thread to sleep based on the delay time, and gets it into seconds
+    //Going off of this documenttation for thrd_sleep
+    //https://en.cppreference.com/w/c/thread/thrd_sleep
+    thrd_sleep(&(struct timespec){.tv_sec=delay/1E-6}, NULL);
+}
 
 int secure_memcmp(uint8_t *a, uint8_t *b, size_t len);
